1bb2edd6489bd76ac8ae7a0862913dd8c24e6f55
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2023-05-17 20:55:32 -0500

    Initial commit

---
+++ Get-LAPSPassword.ps1
@@ -0,0 +1,229 @@
+﻿<#
+============================================================================================================================
+Script: Get-LAPSPassword
+Author: Smart Ace Designs
+
+Notes:
+Use this script to retrieve the LAPS managed password of a member server in the corporate domain.
+============================================================================================================================
+#>
+
+#region Settings
+$RequiredModules = @("LAPS")
+$SupportContact = "Smart Ace Designs"
+#endregion
+
+#region Assemblies
+Add-Type -AssemblyName System.Windows.Forms
+#endregion
+
+#region Appearance
+[System.Windows.Forms.Application]::EnableVisualStyles()
+#endregion
+
+#region Controls
+$FormMain = New-Object -TypeName System.Windows.Forms.Form
+$GroupBoxMain = New-Object -TypeName System.Windows.Forms.GroupBox
+$LabelServerName = New-Object -TypeName System.Windows.Forms.Label
+$TextBoxServerName = New-Object -TypeName System.Windows.Forms.TextBox
+$LabelExpirationDate = New-Object -TypeName System.Windows.Forms.Label
+$TextBoxExpirationDate = New-Object -TypeName System.Windows.Forms.TextBox
+$LabelPassword = New-Object -TypeName System.Windows.Forms.Label
+$TextBoxPassword = New-Object -TypeName System.Windows.Forms.TextBox
+$ButtonRun = New-Object -TypeName System.Windows.Forms.Button
+$ButtonClose = New-Object -TypeName System.Windows.Forms.Button
+$StatusStripMain = New-Object -TypeName System.Windows.Forms.StatusStrip
+$ToolStripStatusLabelMain = New-Object -TypeName System.Windows.Forms.ToolStripStatusLabel
+$ErrorProviderMain = New-Object -TypeName System.Windows.Forms.ErrorProvider
+#endregion
+
+#region Forms
+$ShowFormMain =
+{
+    $FormMain.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon((Get-Process -Id $PID).Path)
+    $FormMain.Text = "Get LAPS Password"
+    $FormMain.ClientSize = New-Object -TypeName System.Drawing.Size(330,260)
+    $FormMain.Font = New-Object -TypeName System.Drawing.Font("MS Sans Serif",8)
+    $FormMain.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
+    $FormMain.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle
+    $FormMain.MaximizeBox = $false
+    $FormMain.AcceptButton = $ButtonRun
+    $FormMain.CancelButton = $ButtonClose
+    $FormMain.Add_Shown($FormMain_Shown)
+
+    $GroupBoxMain.Location = New-Object -TypeName System.Drawing.Point(10,5)
+    $GroupBoxMain.Size = New-Object -TypeName System.Drawing.Size(310,180)
+    $FormMain.Controls.Add($GroupBoxMain)
+
+    $LabelServerName.Location = New-Object -TypeName System.Drawing.Point(15,15)
+    $LabelServerName.AutoSize = $true
+    $LabelServerName.Text = "Server Name:"
+    $GroupBoxMain.Controls.Add($LabelServerName)
+
+    $TextBoxServerName.Location = New-Object -TypeName System.Drawing.Point(15,35)
+    $TextBoxServerName.Size = New-Object -TypeName System.Drawing.Size(280,20)
+    $TextBoxServerName.TabIndex = 0
+    $TextBoxServerName.CharacterCasing = [System.Windows.Forms.CharacterCasing]::Upper
+    $TextBoxServerName.MaxLength = 15
+    $TextBoxServerName.Add_TextChanged($TextBoxServerName_TextChanged)
+    $GroupBoxMain.Controls.Add($TextBoxServerName)
+
+    $LabelExpirationDate.Location = New-Object -TypeName System.Drawing.Point(15,70)
+    $LabelExpirationDate.AutoSize = $true
+    $LabelExpirationDate.Text = "Password Expiration Date:"
+    $GroupBoxMain.Controls.Add($LabelExpirationDate)
+
+    $TextBoxExpirationDate.Location = New-Object -TypeName System.Drawing.Point(30,90)
+    $TextBoxExpirationDate.Size = New-Object -TypeName System.Drawing.Size(260,20)
+    $TextBoxExpirationDate.BorderStyle = "None"
+    $TextBoxExpirationDate.ReadOnly = $true
+    $TextBoxExpirationDate.Text = "Waiting..."
+    $GroupBoxMain.Controls.Add($TextBoxExpirationDate)
+
+    $LabelPassword.Location = New-Object -TypeName System.Drawing.Point(15,125)
+    $LabelPassword.AutoSize = $true
+    $LabelPassword.Text = "Administrator Password:"
+    $GroupBoxMain.Controls.Add($LabelPassword)
+
+    $TextBoxPassword.Location = New-Object -TypeName System.Drawing.Point(30,145)
+    $TextBoxPassword.Size = New-Object -TypeName System.Drawing.Size(260,20)
+    $TextBoxPassword.BorderStyle = "None"
+    $TextBoxPassword.ReadOnly = $true
+    $TextBoxPassword.Text = "Waiting..."
+    $TextBoxPassword.Add_DoubleClick($TextBoxPassword_DoubleClick)
+    $GroupBoxMain.Controls.Add($TextBoxPassword)
+
+    $ButtonRun.Location = New-Object -TypeName System.Drawing.Point(155,200)
+    $ButtonRun.Size = New-Object -TypeName System.Drawing.Size(75,25)
+    $ButtonRun.TabIndex = 100
+    $ButtonRun.Text = "Run"
+    $ButtonRun.Enabled = $fase
+    $ButtonRun.Add_Click($ButtonRun_Click)
+    $FormMain.Controls.Add($ButtonRun)
+
+    $ButtonClose.Location = New-Object -TypeName System.Drawing.Point(245,200)
+    $ButtonClose.Size = New-Object -TypeName System.Drawing.Size(75,25)
+    $ButtonClose.TabIndex = 101
+    $ButtonClose.Text = "Close"
+    $ButtonClose.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+    $FormMain.Controls.Add($ButtonClose)
+
+    $StatusStripMain.SizingGrip = $false
+    $StatusStripMain.Font = New-Object -TypeName System.Drawing.Font("MS Sans Serif",8)
+    [void]$StatusStripMain.Items.Add($ToolStripStatusLabelMain)
+    $FormMain.Controls.Add($StatusStripMain)
+
+    [void]$FormMain.ShowDialog()
+    $FormMain.Dispose()
+}
+#endregion
+
+#region Handlers
+$FormMain_Shown =
+{
+    $ToolStripStatusLabelMain.Text = "Ready"
+    $StatusStripMain.Update()
+    $FormMain.Activate()
+}
+
+$TextBoxServerName_TextChanged =
+{
+    $ToolStripStatusLabelMain.Text = "Ready"
+    $StatusStripMain.Update()
+    $ErrorProviderMain.Clear()
+    $ButtonRun.Enabled = $true
+    $TextBoxPassword.Text = "Waiting..."
+    $TextBoxExpirationDate.Text = "Waiting..."
+    $TextBoxPassword.Font = New-Object -TypeName System.Drawing.Font("Microsoft Sans Serif",8)
+
+    if ($TextBoxServerName.TextLength -eq 0)
+    {
+        $ButtonRun.Enabled = $false
+    }
+    elseif ($TextBoxServerName.Text -match "[^a-z0-9A-Z\-]")
+    {
+        $ErrorProviderMain.SetIconPadding($TextBoxServerName,-20)
+        $ErrorProviderMain.SetError($TextBoxServerName,"The server name contains an invalid character.")
+        $ButtonRun.Enabled = $false
+        $ToolStripStatusLabelMain.Text = "Not ready...please correct error"
+        $StatusStripMain.Update()
+    }
+}
+
+$TextBoxPassword_DoubleClick =
+{
+    Set-Clipboard -Value $TextBoxPassword.Text
+    [void][System.Windows.Forms.MessageBox]::Show(
+        "LAPS password copied to clipboard.`n`nPlease manually clear your clipboard contents when the password is no longer needed.",
+        "Warning",
+        [System.Windows.Forms.MessageBoxButtons]::OK,
+        [System.Windows.Forms.MessageBoxIcon]::Warning
+    )
+}
+
+$ButtonRun_Click = 
+{
+    $ToolStripStatusLabelMain.Text = "Working...please wait"
+    $FormMain.Controls | Where-Object {$PSItem -isnot [System.Windows.Forms.StatusStrip]} | ForEach-Object {$PSItem.Enabled = $false}
+    [System.Windows.Forms.Application]::DoEvents()
+
+    try
+    {
+        $ServerName = ($TextBoxServerName.Text).Trim()
+        if ($Results = Get-LapsADPassword -AsPlainText -Identity  $ServerName -WarningAction SilentlyContinue)
+        {
+            $TextBoxExpirationDate.Text = $Results.ExpirationTimeStamp
+            $TextBoxPassword.Font = New-Object -TypeName System.Drawing.Font("Microsoft Sans Serif",16)
+            $TextBoxPassword.Text = $Results.Password
+        }
+        else
+        {
+            $TextBoxExpirationDate.Text = "No LAPS data found..."
+            $TextBoxPassword.Font = New-Object -TypeName System.Drawing.Font("Microsoft Sans Serif",8)
+            $TextBoxPassword.Text = "No LAPS data found..."
+        }
+    }
+    catch
+    {
+        [void][System.Windows.Forms.MessageBox]::Show(
+            $PSItem.Exception.Message + "`n`nContact $SupportContact for technical support.",
+            "Exception",
+            [System.Windows.Forms.MessageBoxButtons]::OK,
+            [System.Windows.Forms.MessageBoxIcon]::Warning
+        )
+        $FormMain.Close()
+    }
+
+    $FormMain.Controls | ForEach-Object {$PSItem.Enabled = $true}
+    $TextBoxServerName.Focus()
+    $ToolStripStatusLabelMain.Text = "Ready"
+    $StatusStripMain.Update()
+}
+#endregion
+
+#region Main
+$MissingModules = @()
+foreach ($Module in $RequiredModules)
+{
+    if ($RequiredModules -notcontains (Get-Module -ListAvailable -Name $Module | Get-Unique)) {$MissingModules += (" $([char]8226) " + $Module)}
+}
+
+if ($MissingModules)
+{
+    [void][System.Windows.Forms.MessageBox]::Show(
+        "The following PowerShell modules are required:`n`n$($MissingModules -join "`n")`n`nPlease contact $SupportContact for technical support.",
+        "Requirements",
+        [System.Windows.Forms.MessageBoxButtons]::OK,
+        [System.Windows.Forms.MessageBoxIcon]::Information
+    )
+}
+else
+{
+    [void][System.Windows.Forms.MessageBox]::Show(
+        "This script will display the LAPS password in plain text for a specified domain-joined computer.`n`nPlease use with caution.",
+        "Warning",
+        [System.Windows.Forms.MessageBoxButtons]::OK,
+        [System.Windows.Forms.MessageBoxIcon]::Warning
+    )
+    Invoke-Command -ScriptBlock $ShowFormMain
+}
---
+++ Readme.md
@@ -0,0 +1,64 @@
+# **Get-LAPSPassword**
+
+**Warning:** This ScriptoForm is for demonstration purposes only and not intended for use in a production environment.
+
+## Introduction
+This repository contains all files required to build the **Get-LAPSPassword** ScriptoForm project.  A *ScriptoForm* ("Script Oh Form") is a PowerShell script that generates and displays a [Microsoft Windows Forms](https://learn.microsoft.com/en-us/dotnet/desktop/winforms/overview/?view=netdesktop-7.0#introduction) application that can be used for a specific management or system administration task in a network environment.  A *ScriptoForm project* is the set of files and folders, including the PowerShell script, that can be compiled into an executable file using the Microsoft .NET CLI utility (dotnet.exe) which is available with any [Microsoft .NET SDK](https://dotnet.microsoft.com/en-us/download/dotnet).  Included in the repository is the Build.cs C# file which the compiler will use as the source for the executable, and the Build.csproj C# project file which provides the set of instructions used to compile the executable.
+
+## Purpose
+The **Get-LAPSPassword** script launches a Windows form that provides a method to retreive the local administrator account password ([LAPS](https://learn.microsoft.com/en-us/windows-server/identity/laps/laps-overview)) in plain text for a computer in an Active Directory domain.
+
+**Note:** Caution should be used when executing this script to ensure that the LAPS password for a domain-joined computer is not exposed to unauthorized persons.  If copying the password to your clipboard, the clipboard contents should be manually [cleared](https://support.microsoft.com/en-us/windows/get-help-with-clipboard-30375039-ce71-9fe4-5b30-21b7aab6b13f) after the password is no longer needed.
+
+## Requirements
+- This project supports the following command shells:
+    - [Windows PowerShell 5.1](https://docs.microsoft.com/en-us/powershell/scripting/overview?view=powershell-5.1)
+    - [PowerShell 7.2.x](https://docs.microsoft.com/en-us/powershell/scripting/overview?view=powershell-7.2)
+    - [PowerShell 7.3.x](https://docs.microsoft.com/en-us/powershell/scripting/overview?view=powershell-7.3)
+- This project supports the following Microsoft .NET frameworks:
+    - [Microsoft .NET 4.x](https://dotnet.microsoft.com/en-us/download/dotnet-framework)
+    - [Microsoft .NET 6.x](https://dotnet.microsoft.com/en-us/download/dotnet/6.0)
+    - [Microsoft .NET 7.x](https://dotnet.microsoft.com/en-us/download/dotnet/7.0)
+- This project requires the following PowerShell modules:
+    - [Microsoft LAPS](https://learn.microsoft.com/en-us/windows-server/identity/laps/laps-overview)
+- This project requires that LAPS has been enabled in the Active Directory system in your environment.
+- This project requires that script be run on a domain joined computer running a [supported OS](https://learn.microsoft.com/en-us/windows-server/identity/laps/laps-overview#windows-laps-supported-platforms-and-azure-ad-laps-preview-status).
+
+## Compile Instructions
+Perform the following prerequisite steps:
+- Install the [Microsoft .NET 7.x SDK](https://dotnet.microsoft.com/en-us/download/dotnet/7.0) on your development machine.
+- Clone the repository to your development machine.
+
+Use any of the below workflows to create an executable file of the PowerShell script that is compatible with the framework version specified:
+
+Microsoft .NET 4.x Framework
+- Open a supported command shell and navigate to the *Build* subdirectory in your local repository directory.
+- Run the following command from within your *Build* subdirectory:<br>
+``dotnet publish -f net48 -v q -nologo -o ..\Release\Legacy; dotnet clean -f net48 -v q -nologo``
+- The compiled executable will be created in the *Release\Legacy* subdirectory of your local repository directory.  This location can be changed by modifying the ``-o`` parameter in the above command.
+- The latest [Microsoft .NET 4.x Framework Runtime](https://dotnet.microsoft.com/en-us/download/dotnet-framework/net48) will be required on any computer used to run the executable.
+
+Microsoft .NET 6.x Framework
+- Open a supported command shell and navigate to the *Build* subdirectory in your local repository directory.
+- Run the following command from within your *Build* subdirectory:<br>
+``dotnet publish -f net6.0-windows -v q -nologo -o ..\Release\LTS; dotnet clean -f net6.0-windows -v q -nologo``
+- The compiled executable will be created in the *Release\LTS* subdirectory of your local repository directory.  This location can be changed by modifying the ``-o`` parameter in the above command.
+- The latest [Microsoft .NET 6.x Runtime](https://dotnet.microsoft.com/en-us/download/dotnet/6.0) will be required on any computer used to run the executable.
+
+Microsoft .NET 7.x Framework
+- Open a supported command shell and navigate to the *Build* subdirectory in your local repository directory.
+- Run the following command from within your *Build* subdirectory:<br>
+``dotnet publish -f net7.0-windows -v q -nologo -o ..\Release\STS; dotnet clean -f net7.0-windows -v q -nologo``
+- The compiled executable will be created in the *Release\STS* subdirectory of your local repository directory.  This location can be changed by modifying the ``-o`` parameter in the above command.
+- The latest [Microsoft .NET 7.x Runtime](https://dotnet.microsoft.com/en-us/download/dotnet/7.0) will be required on any computer used to run the executable.
+
+## Executable Notes
+- When the executable file is run it will extract all resource files that were included in the compilation process to a unique temporary extraction directory in the user's profile directory.
+- The executable will attempt to use the latest version of PowerShell discovered on the local machine to execute the extracted script file unless excluded by one of the command-line arguments noted below.  If a version of PowerShell is excluded by a command-line argument then the executable will attempt to use the next latest version of PowerShell discovered on the system.  The executable will always default to using Windows PowerShell if no other versions are available for use.
+- After the PowerShell script execution has completed, all extracted files and the extraction directory are deleted and the executable terminates.
+- The following optional command-line arguments can be used to control operation of the executable:
+  | Argument          | Purpose                           | Notes                                                       |
+  | ----------------- | --------------------------------- | ------------------------------------------------------------|
+  | -Exclude:ALL      | Exclude PowerShell x.x.x versions | Do not use with other *Exclude* parameters                  |
+  | -Exclude:PS7      | Exclude PowerShell 7.x.x versions | Do not use with other *Exclude* parameters                  |
+  | -Debug            | Show console window               | Use individually or with one additional *Exclude* parameter |
